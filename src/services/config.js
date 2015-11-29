fda1c7ed8b69328644ed2196173e14c3504129b6
Paolo Scanferla <paolo.scanferla@mondora.com>
2015-11-29 23:38:44 +0100

    Load env vars

--- src/services/config.js
+++ src/services/config.js
@@ -1,2 +1,6 @@
+import dotenv from "dotenv";
+
+dotenv.load();
+
 export const KINESIS_STREAM_NAME = process.env.KINESIS_STREAM_NAME;
 export const KINESIS_PARTITION_KEY = "aggretated-readings-api";
